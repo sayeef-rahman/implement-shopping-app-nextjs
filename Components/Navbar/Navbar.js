26b7e07edbf7f25699105884218ca11d832ead47
sayeef-rahman <sayeef.cse@gmail.com>
2022-10-03 23:31:24 +0600

    header & footer component created

--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -1,9 +1,23 @@
-const Navbar = () => {
-    return (
+const Navbar = (props) => {
+  return (
+    <div className="container bg-success">
+      <div>Online Shop</div>
+      <div>
+        <ul>
+          <li>Shop</li>
+          <li>Blogs</li>
+          <li>About</li>
+          <li>Orders</li>
+        </ul>
         <div>
-            
+          <ul>
+            <li>Profile</li>
+            <li>Cart</li>
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
